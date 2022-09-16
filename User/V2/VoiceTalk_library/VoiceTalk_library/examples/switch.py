55501514987b8fb1ea8448758ef961c89d493eac
gggaaammm <gggaaammm7@gmail.com>
2022-09-16 16:08:08 +0800

    update examples

--- User/V2/VoiceTalk_library/VoiceTalk_library/examples/switch.py
+++ User/V2/VoiceTalk_library/VoiceTalk_library/examples/switch.py
@@ -8,7 +8,7 @@ self.D = 'device'
 self.A = {'open':1, 'close':0}
 self.V = None
 # ***[init_content]***
-voicetalk.update_table(type(self).__name__ ,self.A, self.D, self.F, self.V, Language)
+voicetalk.update_table(type(self).__name__ ,self.D, self.A, self.V, Language)
 # ***[runs_content]***
 Token = voicetalk.get_info(type(self).__name__)
 if( Token['D'] == self.D and Token['A'] ==self.A):
